340313886090dbe75305a5eb24148a774de66b62
Ikbal Demirdoven <ikbaldemirdoven@icloud.com>
2023-03-05 21:19:37 +0400

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Ikbal Demirdoven on 2023-03-05.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
